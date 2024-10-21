8f9aa632d4b50e65b8ae0c94a6b350f3ad1149a3
Samuel Short <samuel.short@gmail.com>
2024-10-21 11:11:36 +0900

    Implement basic navigation component

---
+++ src/routes/_components/Navigation.svelte
@@ -0,0 +1,10 @@
+<nav class="fixed w-full flex items-center justify-between p-5">
+  <img src="TechReisLogo.webp" alt="TechReis Company Logo" />
+  <ul class="flex gap-[2.5rem] pr-[1.5625rem]">
+    <li><a href="/">Home</a></li>
+    <li><a href="/Company">Tech Reis</a></li>
+    <li><a href="/Team">Our Team</a></li>
+    <li><a href="/Blog">Blog</a></li>
+    <li><a href="/Contact">Let's Talk</a></li>
+  </ul>
+</nav>
